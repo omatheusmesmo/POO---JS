f8de30ca112ec8a74e6746a49a60b348c86aff4c
Matheus Oliveira <matheus.6148@gmail.com>
2022-10-02 18:31:16 -0300

    metodos iniciados

--- javascript/js/script1.js
+++ javascript/js/script1.js
@@ -1,17 +1,43 @@
 class Produto {
 
     constructor(){
-        this.id = 0;
-        this.nomeProduto = '';
-        this.valor = 0;
+        this.id = 1;
+        this.arrayProdutos = {};
+    }
+
+    salvar() {
+        let produto = this.lerDados();
+
+        this.validaCampo(produto);
+        
+        console.log(produto);
+    }
+
+    lerDados(){
+        let produto = {}
+
+        produto.id = this.id; 
+        produto.nomeProduto = document.getElementById('produto').value;
+        produto.preco = document.getElementById('preco').value;
+
+        return produto;
+    }
+
+    validaCampo(produto){
+        let msg = '';
+
+        if(produto.nomeProduto == '') {
+            msg += 'Informe o nome do Produto \n';
+        }
+
+        if(produto.preco == '') {
+            msg += 'Informe o preço do Produto \n';
+        }
 
     }
 
-    adicionar() {
-        alert("Vamos adicionar um produto");
-    }
-    excluir() {
-        alert("Item deletado!");
+    cancelar() {
+        
     }
 }
 
